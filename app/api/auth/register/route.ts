29b12335093f8ba62561f6c57c52ed7bed9f12da
riponmondalbd <riponmondal149@gmail.com>
2026-09-07 14:41:30 +0600

    feat(auth): add email existence check during user registration

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,11 +1,14 @@
+import { db } from "@/prisma/db";
 import { registerSchema } from "@/validations/auth.schema";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
 
+  // Validate the request body against the registerSchema
   const result = registerSchema.safeParse(body);
 
+  // If validation fails, return a 400 response with the validation errors
   if (!result.success) {
     return NextResponse.json(
       {
@@ -15,4 +18,21 @@ export async function POST(request: NextRequest) {
       { status: 400 },
     );
   }
+
+  const { name, username, email, password } = result.data;
+
+  // Check if the email already exists in the database
+  const existingEmail = await db.orm.public.User.first({ email });
+
+  if (existingEmail) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Email already exists",
+      },
+      { status: 400 },
+    );
+  }
+
+  // Check if the username already exists in the database
 }
