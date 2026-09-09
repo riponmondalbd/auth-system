87ad48aba6aa79a2bf779779d9e46faba35ddfb5
riponmondalbd <riponmondal149@gmail.com>
2026-09-09 07:40:40 +0600

    feat(auth): add functions for generating and verifying JWT refresh tokens

--- lib/jwt.ts
+++ lib/jwt.ts
@@ -9,12 +9,26 @@ const ACCESS_TOKEN_EXPIRES_IN = process.env
 const REFRESH_TOKEN_EXPIRES_IN = process.env
   .REFRESH_TOKEN_EXPIRES_IN as SignOptions["expiresIn"];
 
+//   Generate a JWT access token
 export function generateAccessToken(payload: AuthTokenPayload) {
   return jwt.sign(payload, ACCESS_TOKEN_SECRET, {
     expiresIn: ACCESS_TOKEN_EXPIRES_IN,
   });
 }
 
+// Verify a JWT access token
 export function verifyAccessToken(token: string): AuthTokenPayload {
   return jwt.verify(token, ACCESS_TOKEN_SECRET) as AuthTokenPayload;
+}
+
+//  Generate a JWT refresh token
+export function generateRefreshToken(payload: AuthTokenPayload) {
+  return jwt.sign(payload, REFRESH_TOKEN_SECRET, {
+    expiresIn: REFRESH_TOKEN_EXPIRES_IN,
+  });
+}
+
+// Verify a JWT refresh token
+export function verifyRefreshToken(token: string): AuthTokenPayload {
+  return jwt.verify(token, REFRESH_TOKEN_SECRET) as AuthTokenPayload;
 }
