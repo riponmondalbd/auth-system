f77f833f94edc59e74bcad42412ec6596d7e6de8
riponmondalbd <riponmondal149@gmail.com>
2026-09-09 07:49:14 +0600

    feat(auth): add setAuthCookies function for managing access and refresh tokens

---
+++ lib/cookie.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export function setAuthCookies(
+  response: NextResponse,
+  accessToken: string,
+  refreshToken: string,
+) {
+  response.cookies.set("accessToken", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    path: "/",
+    maxAge: 15 * 60, // 15 minutes
+  });
+
+  response.cookies.set("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    path: "/",
+    maxAge: 7 * 24 * 60 * 60, // 7 days
+  });
+}
