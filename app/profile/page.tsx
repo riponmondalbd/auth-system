bb57a890dbf28e4588b7af1de41e8631578ed2ae
riponmondalbd <riponmondal149@gmail.com>
2026-09-24 01:56:44 +0600

    feat(profile): create ProfilePage component with user data fetching and display

---
+++ app/profile/page.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+type User = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  image: string | null;
+  role: "USER" | "ADMIN";
+  isVerified: boolean;
+  createdAt: string;
+  updatedAt: string;
+};
+
+const ProfilePage = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    async function fetchProfile() {
+      try {
+        const response = await fetch("/api/users/profile");
+
+        const data = await response.json();
+
+        if (!response.ok) {
+          setMessage(data.message || "Failed to fetch profile.");
+          return;
+        }
+
+        setUser(data.user);
+      } catch (error) {
+        console.error("Error fetching profile:", error);
+        setMessage("An error occurred while fetching profile.");
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchProfile();
+  }, []);
+
+  if (loading) {
+    return (
+      <main className="flex min-h-screen items-center justify-center">
+        <p>Loading profile...</p>
+      </main>
+    );
+  }
+
+  if (message) {
+    return (
+      <main className="flex min-h-screen items-center justify-center">
+        <p className="text-red-500">{message}</p>
+      </main>
+    );
+  }
+
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <main className="mt-6 space-y-4 rounded-lg border p-6">
+      {/* Profile Information name */}
+      <div>
+        <p className="text-sm text-gray-600">Name</p>
+
+        <p className="font-medium">{user.name}</p>
+      </div>
+
+      {/* Profile Information username */}
+      <div>
+        <p className="text-sm text-gray-600">Username</p>
+
+        <p className="font-medium">@{user.username}</p>
+      </div>
+
+      {/* Profile Information email */}
+      <div>
+        <p className="text-sm text-gray-600">Email</p>
+
+        <p className="font-medium">{user.email}</p>
+      </div>
+
+      {/* Profile Information role */}
+      <div>
+        <p className="text-sm text-gray-600">Role</p>
+
+        <p className="font-medium">{user.role}</p>
+      </div>
+
+      {/* Profile Information email status */}
+      <div>
+        <p className="text-sm text-gray-600">Email Status</p>
+        <p className="font-medium">
+          {user.isVerified ? "Verified" : "Not Verified"}
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default ProfilePage;
